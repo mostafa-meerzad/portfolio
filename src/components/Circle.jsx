98a2faa26da97acb76efe5106e5f944224a96ac6
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-21 20:58:43 +0430

    Create design icons

---
+++ src/components/Circle.jsx
@@ -0,0 +1,17 @@
+// import { PropTypes } from "react";
+const Circle = ({className}) => {
+  return (
+    <svg className={className}
+      width="114"
+      height="114"
+      viewBox="0 0 114 114"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="57" cy="57" r="56.5" stroke="#929292" />
+    </svg>
+  );
+};
+
+// Circle.propTypes = {className: PropTypes.string};
+export default Circle;
