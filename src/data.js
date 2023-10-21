e98bce09f5d6929f2f556604da2cc4a5dcbd47b2
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-21 21:00:47 +0430

    Separate sections data in a single file

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const technologies = [
+  ["html", "css"],
+  ["javascript", "react"],
+  ["gsap", "tailwindcss"],
+  ["styled-components", "react router dom"],
+  ["framer-motion", "react hook form"],
+];
+
+export { technologies };
