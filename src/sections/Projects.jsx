db30ea97052d7d610b9f2b77e102042a721e98a3
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-22 15:39:34 +0430

    Add the design rings to the projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,9 +1,13 @@
 import Project from "../components/Project";
+import Rings from "../components/Rings";
 import { projects } from "../data";
 
 const Projects = () => {
   return (
-    <section className="container my-24">
+    <section className="container my-24 relative">
+      <Rings
+        className={"absolute -top-[5%] -right-[45%] scale-105 opacity-70 -z-10"}
+      />
       <h2 className="text-5xl my-12">projects</h2>
       {projects.map((project) => (
         <Project {...project} key={project.name} />
