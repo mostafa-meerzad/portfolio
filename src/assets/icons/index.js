3d89db3558111fec6580e39461bc2ec02674677c
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-23 18:01:36 +0430

    Add social-media links and icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import linkedin from "./linkedin-svgrepo-com.svg";
+import whatsapp from "./whatsapp-128-svgrepo-com.svg";
+import github from "./github-142-svgrepo-com.svg";
+import instagram from ".//instagram-svgrepo-com.svg";
+
+export { linkedin, whatsapp, github, instagram };
