fcdf6466f479144e801235ed16ebb1ab2d3c4778
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-25 18:30:20 +0430

    Complete tablet view of the footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,13 +2,13 @@ import { socialMediaLinks } from "../data";
 
 const Footer = () => {
   return (
-    <section className="container pt-8 pb-4 bg-slightly-dark-gray text-center">
+    <section className="container pt-8 pb-4 bg-slightly-dark-gray text-center md:text-start relative">
       <h2 className="text-3xl ">Contact</h2>
-      <p className="text-gray-300">
+      <p className="text-gray-300 md:max-w-sm">
         I would love to hear about your project and how i could help.
       </p>
 
-      <ul className="flex justify-center items-center gap-4 mt-8 mb-4">
+      <ul className="flex justify-center items-center gap-4 mt-8 mb-4 md:absolute md:right-12 bottom-0 md:top-0 md:gap-8">
         {socialMediaLinks.map(({ name, img, url }) => (
           <li key={name}>
             <a href={url}>
