1087ee9c359fddc227e32a9254b4217dccdc1c78
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-10-26 12:21:37 +0430

    Fix static styles of hero section that was causing problems in the desktop view

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,17 +4,17 @@ import Rings from "../components/Rings";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col items-center  w-full relative md:flex-row-reverse md:pr-0  wrapper">
+    <section className="flex flex-col items-center  w-full relative md:flex-row max-lg:pr-0  wrapper ">
       <img
-        className=" max-sm:w-2/3 sm:w-1/2 md:order-2 "
+        className=" max-sm:w-2/3 sm:w-1/2 md:order-2 max-md:m-auto max-lg:ml-auto"
         src={mainImg}
         alt="Mostafa-image"
       />
       <Rings
-        className={"absolute -left-[70%] top-4 opacity-70 md:-left-[10%]"}
+        className={"absolute -left-[70%] top-4 opacity-70 md:-left-[10%] -z-10"}
       />
 
-      <div className="mt-8  md:absolute md:w-[60%] md:left-12">
+      <div className="mt-8 md:w-[60%] max-md:relative max-lg:absolute">
         <h1 className="font-News text-[2.5rem] leading-10  text-center md:text-start md:text-[3.5rem] md:leading-[3.8rem] md:mb-8">
           <span>Nice to meet you!</span>
           <br />
