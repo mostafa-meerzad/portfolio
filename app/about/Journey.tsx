526f60156e718781b7c9580e12498f02aa902e3a
mostafa <mostafameerzad@gmail.com>
2025-07-12 18:44:22 +0430

    add: myJourney section

---
+++ app/about/Journey.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+import selfBuilt from "../assets/self-built.png";
+
+const Journey = () => {
+  return (
+    <div
+      className="grid grid-cols-1 md:grid-cols-[1fr_0.8fr] md:grid-rows-[0.1fr_.5fr] gap-5 justify-items-center md:justify-items-start md:items-center
+    "
+    >
+      <h2 className="col-start-1 col-end-2 md:col-end-3 md:row-start-1 md:row-end-2 text-4xl font-semibold">
+        My Journey
+      </h2>
+
+      <p className="col-start-1 col-end-2 md:row-start-2 md:row-end-3 text-center md:text-start text-lg px-5 md:px-0 md:self-start">
+        My journey began in 2019 when I got accepted to Kabul University for
+        software engineering. Then came challenges: COVID-19, a university
+        attack, and the collapse of the government in 2021. I left college, but
+        not my dream. With determination, I turned to the internet and taught
+        myself full-stack development using FreeCodeCamp, YouTube, and tons of
+        projects. Now, I’ve built 150+ projects, earned multiple certifications,
+        and landed an internship, all self-driven.
+      </p>
+      <Image
+        src={selfBuilt}
+        alt="self built"
+        className="col-start-1 md:col-start-2 col-end-2 md:col-end-3 md:row-start-1 md:row-end-3 w-auto h-[380px] object-contain lg:justify-self-end rounded-xl"
+      />
+    </div>
+  );
+};
+
+export default Journey;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutMe from "./AboutMe";
+import Journey from "./Journey";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="grid gap-16">
       <AboutMe />
+      <Journey/>
     </div>
   );
 };
