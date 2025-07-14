19d912d122f032e2fe5bd44c7bd91cb2f952d854
mostafa <mostafameerzad@gmail.com>
2025-07-14 18:24:50 +0430

    add: projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import ProjectsList from "@/components/ui/ProjectsList";
 
 const ProjectsPage = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="grid  pt-8 md:pt-16 pb-10 gap-12">
+      <h2 className=" justify-self-center md:justify-self-start text-4xl font-semibold">
+        Projects
+      </h2>
+      <ProjectsList />
+    </div>
+  );
 };
 
 export default ProjectsPage;
