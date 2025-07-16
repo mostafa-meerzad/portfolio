08ec8df8a1a0bba0d54f9c3acdb435d11ce7795a
mostafa <mostafameerzad@gmail.com>
2025-07-16 14:47:05 +0430

    refactor: limit social media in footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -12,18 +12,27 @@ const Footer = () => {
     <footer className="flex justify-between items-center border-t background p-5 px-3 md:px-8 lg:px-12">
       <h2 className="font-semibold">Mostafa Meerzad</h2>
       <ul className="flex justify-center gap-5">
-        {socialMedia.map(({ Img, hover, href }) => (
-          <li key={href}>
-            <Link href={href}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Img className="size-[25px]" />
-                </TooltipTrigger>
-                <TooltipContent className="font-semibold">{hover}</TooltipContent>
-              </Tooltip>
-            </Link>
-          </li>
-        ))}
+        {socialMedia
+          .slice(0, 4)
+          .map(({ Img, hover, href, opts: { ariaLabel, rel, target } }) => (
+            <li key={href}>
+              <Link
+                href={href}
+                rel={rel}
+                target={target}
+                aria-label={ariaLabel}
+              >
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Img className="size-[25px]" />
+                  </TooltipTrigger>
+                  <TooltipContent className="font-semibold">
+                    {hover}
+                  </TooltipContent>
+                </Tooltip>
+              </Link>
+            </li>
+          ))}
       </ul>
     </footer>
   );
