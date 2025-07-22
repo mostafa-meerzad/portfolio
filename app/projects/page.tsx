62f1813afd5548b95e33cd3c23a48f5ba54dc082
mostafa <mostafameerzad@gmail.com>
2025-07-22 10:10:01 +0430

    add: animations to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,18 @@
+"use client";
 import ProjectsList from "@/components/ui/ProjectsList";
+import { motion } from "framer-motion";
 
 const ProjectsPage = () => {
   return (
     <div className="grid  pt-8 md:pt-16 pb-10 gap-12">
-      <h2 className=" justify-self-center md:justify-self-start h2">
+      <motion.h2
+        initial={{ scale: 0.95, opacity: 0 }}
+        whileInView={{ scale: 1, opacity: 1 }}
+        viewport={{ once: true }}
+        className=" justify-self-center md:justify-self-start h2"
+      >
         Projects
-      </h2>
+      </motion.h2>
       <ProjectsList />
     </div>
   );
