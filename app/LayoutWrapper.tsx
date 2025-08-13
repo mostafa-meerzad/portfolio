9e2b3c22c5fbd341373db9fd170fcaaf5d20c61f
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-08-13 12:49:20 +0430

    add: separate images for mobile and desktop

--- app/LayoutWrapper.tsx
+++ app/LayoutWrapper.tsx
@@ -13,13 +13,14 @@ export default function LayoutWrapper({ children }: Props) {
   // Define background classes based on pathname
   const backgroundClass = (() => {
     if (pathname === "/")
-      return "bg-[url('/bg-home-light.webp')] dark:bg-[url('/bg-home-dark.webp')]";
+      // return "bg-[url('/bg-home-light.webp')] dark:bg-[url('/bg-home-dark.webp')] bg-[80%_100%] ";
+      return "bg-[url('/bg-home-light-mobile.jpg')] dark:bg-[url('/bg-home-dark-mobile.jpg')] md:bg-[url('/bg-home-light.webp')] md:dark:bg-[url('/bg-home-dark.webp')] bg-contain bg-left ";
     if (pathname.startsWith("/about"))
-      return "bg-[url('/bg-about-light.webp')] dark:bg-[url('/bg-about-dark.webp')]";
+      return "bg-[url('/bg-about-light-mobile.jpg')] dark:bg-[url('/bg-about-dark-mobile.jpg')] md:bg-[url('/bg-about-light.webp')] md:dark:bg-[url('/bg-about-dark.webp')] bg-contain bg-left";
     if (pathname.startsWith("/projects"))
-      return "bg-[url('/bg-projects-light.webp')] dark:bg-[url('/bg-projects-dark.webp')]";
+      return "bg-[url('/bg-projects-light-mobile.jpg')] dark:bg-[url('/bg-projects-dark-mobile.jpg')] md:bg-[url('/bg-projects-light.webp')] md:dark:bg-[url('/bg-projects-dark.webp')] bg-contain bg-left";
     if (pathname.startsWith("/contact"))
-      return "bg-[url('/bg-contact-light.webp')] dark:bg-[url('/bg-contact-dark.webp')]";
+      return "bg-[url('/bg-contact-light-mobile.jpg')] dark:bg-[url('/bg-contact-dark-mobile.jpg')] md:bg-[url('/bg-contact-light.webp')] md:dark:bg-[url('/bg-contact-dark.webp')] bg-contain bg-left";
     return "bg-[url('/bg-default.webp')]";
   })();
 
